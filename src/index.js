c7fe49b42879b86f7277e831077c3dcb33b54f1b
mashnello <mashnenko@ukr.net>
2017-02-21 22:03:35 +0200

    add webpack.dev.config.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
-import './styles/app.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './styles/app.scss';
 
-render(<App/>, document.getElementById('main'))
+ReactDOM.render(<App/>, document.getElementById('main'));
