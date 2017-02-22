ccf61d96f12b54b25a7e9940bab3c95771d5ce4e
mashnello <mashnenko@ukr.net>
2017-02-22 23:32:01 +0200

    Table component

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,14 @@
 import React, { Component } from 'react';
 
+import Table from './Table';
+
 export default class App extends React.Component {
     render() {
         return (
             <div className="container">
-      <h2>Hello from the Life Counter!</h2>
-      <table className="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th>Day Of Week/Activity</th>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
-            <th>Sunday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Gym</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Coding</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Bowling</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Swimming</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+                <h2>Hello from the Life Counter!</h2>
+                <Table />
+            </div>
         );
     }
 };
---
+++ src/components/Table.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+export default class Table extends React.Component {
+    render() {
+        return (
+            <table className="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Day Of Week/Activity</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Gym</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Coding</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Bowling</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Swimming</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+            </table>
+        );
+    }
+};
