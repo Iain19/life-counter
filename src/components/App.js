e4e24acea57afed711d9eefca02946c39edc0ab1
mashnello <mashnenko@ukr.net>
2017-02-22 23:24:48 +0200

    basic markup to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,65 @@ import React, { Component } from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <h1>Hello from the Life Counter!</h1>
+            <div className="container">
+      <h2>Hello from the Life Counter!</h2>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Day Of Week/Activity</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Gym</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Coding</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Bowling</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Swimming</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
         );
     }
 };
