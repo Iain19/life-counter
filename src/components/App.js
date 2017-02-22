6037a36601202bf868c42050acf8c679d545fa52
Igor Khudik <js.web23@gmail.com>
2017-02-22 22:40:37 +0200

    Fix HMR in App component; modify gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-    return (
-        <h1>Hello from the Life Counter!</h1>
-    );
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello from the Life Counter!</h1>
+        );
+    }
 };
-
-export default App;
