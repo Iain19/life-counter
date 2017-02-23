b5cc47ae5433b492c20b7ca610be1b019dd312e5
mashnello <mashnenko@ukr.net>
2017-02-23 22:32:21 +0200

    redux basic setup

---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as Actions from '../actions/index';
+
+import ActivityTable from '../components/ActivityTable';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="container">
+                <h2>Hello from the Life Counter!</h2>
+                <ActivityTable />
+            </div>
+        );
+    }
+};
+
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './containers/App';
+import reducers from './reducers';
 import './styles/app.scss';
 
-ReactDOM.render(<App/>, document.getElementById('main'));
+const createStoreWIthMiddleware = applyMiddleware()(createStore);
+const store = createStoreWIthMiddleware(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>,
+        <App />,
+    </Provider>,
+    document.getElementById('main')
+);
